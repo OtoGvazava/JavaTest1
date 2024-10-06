d4e6f8b66d5aeb8b64a029d21f437e39e86758cf
Otari Gvazava <otarigvazava@Otaris-MacBook-Pro.local>
2024-10-06 12:58:48 +0400

    upd

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test1");
         System.out.println("Test2");
-        System.out.println("Test3");
+        System.out.println("Test4");
+        System.out.println("Test19");
     }
 }
